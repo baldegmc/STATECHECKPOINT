c68d4705662c01fe56ba675768456d5767fab7d0
baldegmc <bowal15@yahoo.fr>
2023-12-30 16:19:16 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+
+
+
 import './App.css';
+import React from 'react';
+//import ReactDOM from 'react-dom/client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    Person: {
+      fullName: "Naruto Uzumaki",
+      bio: "I just love ramen",
+      imgSrc: "./naruto.jpeg",
+      profession: "7th Hokage"
+    },
+    show: true
+  };
+
+  handleShowPerson = () => {
+    this.setState({
+      ...this.state,
+      show: !this.state.show
+    });
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.show && (
+          <>
+            <h1>{this.state.Person.fullName}</h1>
+            <h1>{this.state.Person.bio}</h1>
+            <img src={this.state.Person.imgSrc} alt="Naruto"></img>
+            <h1>{this.state.Person.profession}</h1>
+            <br></br>
+          </>
+        )}
+
+        <button onClick={this.handleShowPerson}>click here</button>
+      </>
+    );
+  }
 }
-
+/*const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);*/
 export default App;
